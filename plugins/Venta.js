e7e24b971ff050772cffef6a336a2fba9f7f9e9b
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-04-27 09:37:00 -0500

    Create Venta.js

---
+++ plugins/Venta.js
@@ -0,0 +1,30 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `NETFLIX 
+MAX
+STAR MAX 
+DISNEY 
+CRUNCHYROLL
+PARAMOUNT 
+DIRETV
+MAGIS TV
+PRIME VIDEO
+YT PREMIUN
+SPOTIFY
+APPLE MUSIC & APPLE TV
+VIX
+TIDAL
+PLEX TV
+CANVA
+          ` 
+}
+handler.command = /^(ventas)$/i
+handler.register = true
+handler.group = true
+handler.admin = true
+export default handler
